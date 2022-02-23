1d934266dee90a8817995d96efdaf87a9148ab35
zyl <youluzheng@foxmail.com>
2022-02-23 10:34:18 +0800

    Offer5替换字符串

---
+++ src/main/java/cn/youluzheng/leetcode/字符串/O5.java
@@ -0,0 +1,15 @@
+package cn.youluzheng.leetcode.字符串;
+
+public class O5 {
+    public static String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/cn/youluzheng/leetcode/字符串/O5Test.java
@@ -0,0 +1,13 @@
+package cn.youluzheng.leetcode.字符串;
+
+import org.junit.jupiter.api.Test;
+
+class O5Test {
+
+    @Test
+    void replaceSpace() {
+        String s = "We are happy.";
+        String actual = O5.replaceSpace(s);
+        System.out.println(actual);
+    }
+}
