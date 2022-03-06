6a8f7bd100f55d14371e9ad9bd0ee1d3a2036ef2
zyl <youluzheng@foxmail.com>
2022-03-06 13:35:39 +0800

    235二叉搜索树的最近公共祖先

---
+++ src/main/java/cn/youluzheng/leetcode/二叉树/P235.java
@@ -0,0 +1,19 @@
+package cn.youluzheng.leetcode.二叉树;
+
+
+import cn.youluzheng.leetcode.util.TreeNode;
+
+public class P235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
