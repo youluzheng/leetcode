b4390ae5449689a491983a5b07fa569a35797712
zyl <youluzheng@foxmail.com>
2022-03-06 14:36:35 +0800

    669修剪二叉搜索树

---
+++ src/main/java/cn/youluzheng/leetcode/二叉树/P669.java
@@ -0,0 +1,20 @@
+package cn.youluzheng.leetcode.二叉树;
+
+import cn.youluzheng.leetcode.util.TreeNode;
+
+public class P669 {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        return root;
+    }
+}
