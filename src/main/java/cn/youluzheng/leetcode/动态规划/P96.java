68a2477272a9f8026df15e74ddd81a78cebd50bb
zyl <youluzheng@foxmail.com>
2022-03-16 11:20:30 +0800

    96不同的二叉搜索树

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P96.java
@@ -0,0 +1,19 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P96 {
+    public int numTrees(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j <= i - 1; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+}
