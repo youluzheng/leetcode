739adc2a6b46762eee1b538701ae69d377993931
zyl <youluzheng@foxmail.com>
2022-03-19 17:56:26 +0800

    518零钱兑换2

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P518.java
@@ -0,0 +1,14 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P518 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = dp[j] + dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+}
