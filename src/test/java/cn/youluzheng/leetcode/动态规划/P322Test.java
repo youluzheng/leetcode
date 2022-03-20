0fd157603a2b59d1f3d433f200cfd03f36bfbfab
zyl <youluzheng@foxmail.com>
2022-03-20 14:50:47 +0800

    322零钱兑换

---
+++ src/test/java/cn/youluzheng/leetcode/动态规划/P322Test.java
@@ -0,0 +1,11 @@
+package cn.youluzheng.leetcode.动态规划;
+
+import org.junit.jupiter.api.Test;
+
+class P322Test {
+
+    @Test
+    void coinChange() {
+        int actual = new P322().coinChange(new int[]{186, 419, 83, 408}, 6249);
+    }
+}
