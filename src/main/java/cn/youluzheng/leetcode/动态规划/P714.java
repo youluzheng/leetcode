e1f1bf86e9348f0ea3a318afb2163d0546775de3
zyl <youluzheng@foxmail.com>
2022-03-23 16:54:01 +0800

    714买卖股票的最佳时机含手续费

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P714.java
@@ -0,0 +1,16 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P714 {
+    public int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+
+        dp[0][0] = -prices[0] - fee;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[prices.length - 1][1];
+    }
+}
