5a9a168d205ef0a99e50a81fd4b6fd83b9d94826
zyl <youluzheng@foxmail.com>
2022-03-26 20:57:51 +0800

    583两个字符串的删除操作

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P583.java
@@ -0,0 +1,17 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P583 {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return word1.length() + word2.length() - 2 * dp[word1.length()][word2.length()];
+    }
+}
