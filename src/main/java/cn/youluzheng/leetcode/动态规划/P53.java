0aa099314dabf2ba2237a51eceab2a90dca58117
zyl <youluzheng@foxmail.com>
2022-03-26 11:33:33 +0800

    53最大子数组和

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P53.java
@@ -0,0 +1,14 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P53 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
