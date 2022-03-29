2ef126077ddd5f216728b199c033337b99229f9a
zyl <youluzheng@foxmail.com>
2022-03-29 14:47:48 +0800

    516最长回文子序列

---
+++ src/main/java/cn/youluzheng/leetcode/动态规划/P516.java
@@ -0,0 +1,24 @@
+package cn.youluzheng.leetcode.动态规划;
+
+public class P516 {
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i == j) {
+                        dp[i][j] = 1;
+                    } else if (j - i == 1) {
+                        dp[i][j] = 2;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    }
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+}
