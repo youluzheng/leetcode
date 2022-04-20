651142c66d65343919ae4fc650ae04db2e7db1a1
zyl <youluzheng@foxmail.com>
2022-04-20 11:36:50 +0800

    [206]反转链表

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P1_206.java
@@ -0,0 +1,35 @@
+package cn.youluzheng.leetcode.topcode;
+
+import cn.youluzheng.leetcode.util.ListNode;
+
+public class P1_206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode node = head;
+        ListNode next = node.next;
+        node.next = pre;
+        while (next != null) {
+            pre = node;
+            node = next;
+            next = next.next;
+            node.next = pre;
+        }
+        return node;
+    }
+
+    ListNode reverse(ListNode pre, ListNode now) {
+        if (now == null) {
+            return pre;
+        }
+        ListNode next = now.next;
+        now.next = pre;
+        return reverse(now, next);
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        return reverse(null, head);
+    }
+}
