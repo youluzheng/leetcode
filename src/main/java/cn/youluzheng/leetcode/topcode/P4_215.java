117fb2f7eda919d4df798960914e68e4200facf2
zyl <youluzheng@foxmail.com>
2022-04-23 13:13:09 +0800

    [215]数组中的第K个最大元素

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P4_215.java
@@ -0,0 +1,20 @@
+package cn.youluzheng.leetcode.topcode;
+
+import java.util.PriorityQueue;
+
+public class P4_215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+        int i = 0;
+        for (; i < k; i++) {
+            queue.add(nums[i]);
+        }
+        for (; i < nums.length; i++) {
+            if (nums[i] > queue.peek()) {
+                queue.poll();
+                queue.add(nums[i]);
+            }
+        }
+        return queue.peek();
+    }
+}
