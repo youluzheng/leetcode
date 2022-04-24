9b89944b668a4aea1ac41975ee45f0810eccfc47
zyl <youluzheng@foxmail.com>
2022-04-24 14:15:50 +0800

    290周赛

---
+++ src/main/java/cn/youluzheng/leetcode/竞赛/n290/P6041.java
@@ -0,0 +1,31 @@
+package cn.youluzheng.leetcode.竞赛.n290;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class P6041 {
+    public List<Integer> intersection(int[][] nums) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int j = 0; j < nums[0].length; j++) {
+            map.put(nums[0][j], 1);
+        }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if (map.containsKey(nums[i][j])) {
+                    if (map.get(nums[i][j]) == i) {
+                        map.put(nums[i][j], i + 1);
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int key : map.keySet()) {
+            if (map.get(key) == nums.length) {
+                result.add(key);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/cn/youluzheng/leetcode/竞赛/n290/P6042.java
@@ -0,0 +1,33 @@
+package cn.youluzheng.leetcode.竞赛.n290;
+
+public class P6042 {
+
+    boolean isIn(int[] circle, int i, int j) {
+        return (i - circle[0]) * (i - circle[0]) + (j - circle[1]) * (j - circle[1]) <= circle[2] * circle[2];
+    }
+
+    public int countLatticePoints(int[][] circles) {
+        int result = 0;
+        int minI = Integer.MAX_VALUE;
+        int maxI = Integer.MIN_VALUE;
+        int minJ = Integer.MAX_VALUE;
+        int maxJ = Integer.MIN_VALUE;
+        for (int[] ints : circles) {
+            minI = Math.min(minI, ints[0] - ints[2]);
+            maxI = Math.max(maxI, ints[0] + ints[2]);
+            minJ = Math.min(minJ, ints[1] - ints[2]);
+            maxJ = Math.max(maxJ, ints[1] + ints[2]);
+        }
+        for (int i = minI; i <= maxI; i++) {
+            for (int j = minJ; j <= maxJ; j++) {
+                for (int[] circle : circles) {
+                    if (isIn(circle, i, j)) {
+                        result++;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
