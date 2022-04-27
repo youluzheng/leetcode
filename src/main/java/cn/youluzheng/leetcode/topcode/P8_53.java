81fda824c9a268bfcb1110f054bf71ac5651d7ac
zyl <youluzheng@foxmail.com>
2022-04-27 09:23:46 +0800

    [53]最大子数组和

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P8_53.java
@@ -0,0 +1,14 @@
+package cn.youluzheng.leetcode.topcode;
+
+public class P8_53 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
