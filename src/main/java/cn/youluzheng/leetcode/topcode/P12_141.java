69c7eca622c5e222d9c85acd17032e024ac766ad
zyl <youluzheng@foxmail.com>
2022-04-30 20:14:39 +0800

    [141]环形链表

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P12_141.java
@@ -0,0 +1,19 @@
+package cn.youluzheng.leetcode.topcode;
+
+import cn.youluzheng.leetcode.util.ListNode;
+
+public class P12_141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            fast = fast.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
