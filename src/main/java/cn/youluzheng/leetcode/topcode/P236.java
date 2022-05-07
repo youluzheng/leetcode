9af5e6729705e5aaa59a323560208d34baae7874
zyl <youluzheng@foxmail.com>
2022-05-07 09:29:12 +0800

    [236]二叉树的最近公共祖先

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P236.java
@@ -0,0 +1,23 @@
+package cn.youluzheng.leetcode.topcode;
+
+import cn.youluzheng.leetcode.util.TreeNode;
+
+public class P236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        return right;
+    }
+}
