364280b727e885bd19f138a8a28c651f34ab88c2
zyl <youluzheng@foxmail.com>
2022-07-09 11:51:50 +0800

    [112]路径总和

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P112.java
@@ -0,0 +1,16 @@
+package cn.youluzheng.leetcode.topcode;
+
+import cn.youluzheng.leetcode.util.TreeNode;
+
+public class P112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
