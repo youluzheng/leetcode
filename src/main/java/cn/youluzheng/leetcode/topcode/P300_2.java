57e03e1f09ffc0f1092697f570be756adeb408bf
zyl <youluzheng@foxmail.com>
2022-07-17 09:11:00 +0800

    300. 最长上升子序列

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P300_2.java
@@ -0,0 +1,19 @@
+package cn.youluzheng.leetcode.topcode;
+
+public class P300_2 {
+    public int lengthOfLIS(int[] nums) {
+        int max = 1;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
