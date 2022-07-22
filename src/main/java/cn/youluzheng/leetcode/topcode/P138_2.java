36f93d2949c3a7f4810094ae7f96344b89360314
zyl <youluzheng@foxmail.com>
2022-07-22 08:58:33 +0800

    138. 复制带随机指针的链表

---
+++ src/main/java/cn/youluzheng/leetcode/topcode/P138_2.java
@@ -0,0 +1,35 @@
+package cn.youluzheng.leetcode.topcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P138_2 {
+    private class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        map.put(null, null);
+        Node node = head;
+        while (node != null) {
+            map.put(node, new Node(node.val));
+            node = node.next;
+        }
+        node = head;
+        while (node != null) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+        return map.get(head);
+    }
+}
