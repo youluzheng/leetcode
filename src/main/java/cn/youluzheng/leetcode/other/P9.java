80dc0846a0262e2a980088c72dd489ec3aa9d37a
zyl <youluzheng@foxmail.com>
2022-08-10 18:49:21 +0800

    [9]回文数

---
+++ src/main/java/cn/youluzheng/leetcode/other/P9.java
@@ -0,0 +1,18 @@
+package cn.youluzheng.leetcode.other;
+
+public class P9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        }
+        String s = String.valueOf(x);
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
